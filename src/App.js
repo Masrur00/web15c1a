6950890fbaf1b2da9fcf236cb44cf4518de5105c
Masrur Alam <masrur.jsr@gmail.com>
2022-04-12 21:39:14 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { winning } from './components/winning';
+import { useState } from "react";
+
 
 function App() {
+ 
+  const findOver = (val)=> {
+      let str = "";
+      str = Math.round(val/6)+"." + (val%6);  // 50 
+      return str;
+  }
+   
+  const [tracker, setTracker] = useState({ score: 76, wicket: 2, ball: 50 });
+  const addScore = (val) => {
+    setTracker({...tracker, score:tracker.score+val});
+  }
+  
+
+
+  const addBall = (val) => {   
+    setTracker({...tracker, ball:tracker.ball+val});
+  }
+
+  const addWicket = (val) => {   
+    
+      setTracker({...tracker, wicket:tracker.wicket+val});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>India:</h3>
+      <div className="banner">
+        <div>
+          Score:{" "}
+          <h1 className="scoreCount">{tracker.score}</h1>
+        </div>
+
+        <div>
+          Wicket:{" "}
+          <h1 className="wicketCount">
+            {tracker.wicket}
+          </h1>
+        </div>
+
+        <div>
+          Over:{" "}
+          <h1 className="overCount">
+            {(tracker.ball >= 6)? findOver(tracker.ball)  : tracker.ball}
+            {/* // Show Over here in the format: "over.ball" eg: 4.5 means 4th over and 5th ball */}
+            {/* // if 1 more ball is thrown then over is now 5.0
+              // you have to write logic to form this string from current ball number. */}
+
+          </h1>
+        </div>
+      </div>
+
+      <div className="addScore">
+        Add Score
+        {/* these buttons should add the respective amount in the score */}
+        <button className="addScore1" onClick={() => addScore(1)}>Add 1</button>
+        <button className="addScore4" onClick={() => addScore(4)}>Add 4</button>
+        <button className="addScore6" onClick={() => addScore(6)}>Add 6</button>
+      </div>
+
+      <div className="addWicket">
+        Add Wicket
+        {/* Increase the count of wicket */}
+        <button className='w_btn' onClick={() => addWicket(1)}>Add 1 wicket</button>
+      </div>
+
+      <div className="addBall">
+        Add ball
+        {/* Increase the total number of balls thrown here. */}
+        <button className='b_btn' onClick={() => addBall(1)}>Add 1</button>
+      </div>
+
+      {/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
+      <winning score={tracker.score} />
     </div>
   );
 }
---
+++ src/components/winning.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+export const winning = ({score}) => {
+    if(score >= 100){
+    return(
+        <>
+        <h1 className="win">India Win</h1>
+        </>         
+    );}
+    
+}
